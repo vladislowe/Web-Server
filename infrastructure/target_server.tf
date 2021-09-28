127ab3c2d7b198b565fa0c9159cb08741a6bb54d
vladislowe <52048099+vladislowe@users.noreply.github.com>
2021-09-28 16:41:38 +0300

    Add availability zone to target_server.tf

--- infrastructure/target_server.tf
+++ infrastructure/target_server.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "Target_server" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+  availability_zone = "eu-central-1b"
 
   tags = {
     Name = "Target_server"
